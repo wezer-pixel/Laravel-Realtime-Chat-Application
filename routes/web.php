f60d78a676d6074be073544e8eacde32d5e11764
wezer-pixel <jerryjuma104@gmail.com>
2024-03-18 19:51:14 +0300

    chat body component

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Chat\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/chat', Index::class)->name('chat.index');
