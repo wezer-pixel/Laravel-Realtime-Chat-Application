8637647c4ba517dceb6784386f26125a0719c2e9
wezer-pixel <jerryjuma104@gmail.com>
2024-03-19 08:19:43 +0300

    model definitions

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'receiver_id',
+        'sender_id',
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function conversation()
+    {
+        return $this->hasMany(Conversation::class, 'sender_id')->orWhere('receiver_id', $this->id);
+    }
 }
